d9eb17b780d50143d23ef1c72b00d882aafe2c07
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-02-12 21:58:19 +0700

    added some code

--- index.php
+++ index.php
@@ -3,3 +3,15 @@
 namespace App;
 
 require_once './vendor/autoload.php';
+
+use App\Node;
+
+$node = new Node();
+$node->createNode($node, 15);
+$node->insertNode($node, 16);
+$node->insertNode($node, 14);
+$node->insertNode($node, 12);
+$node->insertNode($node, 13);
+$node->insertNode($node, 17);
+
+var_dump($node->right);
