b091e7309a5eab699888591f725d6697493dfd19
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-02-12 20:42:22 +0700

    add nodeExists(Node) method

--- src/Node.php
+++ src/Node.php
@@ -29,4 +29,13 @@ class Node
     public function __construct()
     {
     }
+
+    /**
+     * @param Node $node
+     * @return bool
+     */
+    public function nodeExists(Node $node): bool
+    {
+        return $node != null && $node->value != null;
+    }
 }
