d8eb03381aff2fb9cd9594d545c4f02e65ca3eb5
Alex Loskutov <599181+aloskutov@users.noreply.github.com>
2021-02-12 20:40:54 +0700

    add constructor

--- src/Node.php
+++ src/Node.php
@@ -6,7 +6,27 @@ namespace App;
 
 class Node
 {
+    /**
+     * Node left child
+     * @var Node
+     */
     public Node $left;
+    /**
+     * Node right child
+     * @var Node
+     */
     public Node $right;
+    /**
+     * Node value
+     * @var int
+     */
     public int $value;
+
+    /**
+     * Node constructor.
+     * @param Node|null $node
+     */
+    public function __construct()
+    {
+    }
 }
